a22453709f07c9e960bedaca08e38a68256dfaf4
Aang Muammar Zein <amuammarzein@gmail.com>
2023-05-23 10:10:00 +0700

    Initial Commit

---
+++ fruithockey/fruithockeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  fruithockeyApp.swift
+//  fruithockey
+//
+//  Created by Aang Muammar Zein on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct fruithockeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
